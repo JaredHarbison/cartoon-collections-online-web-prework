8e7fafe83ad9084ce93327a306edbc79cff4462b
Jared Harbison <jared.harbison@gmail.com>
2019-01-06 02:56:49 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,8 +17,8 @@ def long_planeteer_calls(array)
 end 
 
 def find_the_cheese(array)
-  array.find do |cheeses| 
+  array.find do |cheese| 
     cheese == "cheddar" || cheese == "gouda" || cheese == "camembert"
     end 
-end 
+  end 
 
